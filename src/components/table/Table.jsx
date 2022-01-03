aff8acd5ec9826382531f6a26b2944a7a06f49d9
Shahidraza Moulvi <shahidraza.moulvi@aimedatsolutions.com>
2022-01-03 19:18:51 +0530

    refactor: updated table component

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,47 +1,37 @@
 import React from "react";
-import { Table } from "reactstrap";
+import {
+  Table,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  UncontrolledDropdown,
+  Card,
+} from "reactstrap";
+import { get } from "lodash";
+import PropTypes from "prop-types";
+import "./table.scss";
 
-const CustomTable = () => {
-  //   const dataSource = [
-  //     {
-  //       key: "1",
-  //       name: "Mike",
-  //       age: 32,
-  //       address: "10 Downing Street",
-  //     },
-  //     {
-  //       key: "2",
-  //       name: "John",
-  //       age: 42,
-  //       address: "10 Downing Street",
-  //     },
-  //   ];
-
-  //   const columns = [
-  //     {
-  //       title: "Name",
-  //       dataIndex: "name",
-  //       key: "name",
-  //     },
-  //     {
-  //       title: "Age",
-  //       dataIndex: "age",
-  //       key: "age",
-  //     },
-  //     {
-  //       title: "Address",
-  //       dataIndex: "address",
-  //       key: "address",
-  //     },
-  //   ];
+const CustomTable = (props) => {
+  const {
+    tableName,
+    dataSource,
+    columns,
+    withActions,
+    actionList,
+    handleActionSelection,
+  } = props;
 
   const TableHead = () => {
     return (
       <tr>
-        <th>#</th>
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Username</th>
+        {columns?.map((column, index) => {
+          return (
+            <th key={index} className={`${tableName}-${column?.title}`}>
+              {column?.label}
+            </th>
+          );
+        })}
+        {withActions && <th className="table-actions">Actions</th>}
       </tr>
     );
   };
@@ -49,40 +39,110 @@ const CustomTable = () => {
   const TableBody = () => {
     return (
       <>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td>Larry</td>
-          <td>the Bird</td>
-          <td>@twitter</td>
-        </tr>
+        {dataSource?.map((eachRow, rowIndex) => {
+          return (
+            <tr key={rowIndex}>
+              {columns?.map((eachColumn, columnIndex) => {
+                return (
+                  <td
+                    key={columnIndex}
+                    id={`${tableName}${rowIndex}${columnIndex}`}
+                  >
+                    {eachColumn?.render !== undefined
+                      ? eachColumn?.render(eachRow)
+                      : Array.isArray(get(eachRow, eachColumn?.key))
+                      ? get(eachRow, eachColumn?.key).join(", ")
+                      : get(eachRow, eachColumn?.key)}
+                  </td>
+                );
+              })}
+              {withActions && (
+                <td>
+                  <div style={{ paddingLeft: "12px" }}>
+                    <UncontrolledDropdown>
+                      <DropdownToggle className="actions-dropdown-toggle">
+                        <i
+                          className="fas fa-ellipsis-h"
+                          style={{ color: "rgb(134, 146, 176)" }}
+                        />
+                      </DropdownToggle>
+                      <DropdownMenu right>
+                        {actionList?.map((action, index) => {
+                          return (
+                            <>
+                              <DropdownItem
+                                key={index}
+                                className="actions-dropdown-item"
+                                onClick={() =>
+                                  handleActionSelection(action.label)
+                                }
+                              >
+                                <i className={action.icon} /> {action.label}
+                              </DropdownItem>
+                              {action.divider && <DropdownItem divider />}
+                            </>
+                          );
+                        })}
+                      </DropdownMenu>
+                    </UncontrolledDropdown>
+                  </div>
+                </td>
+              )}
+            </tr>
+          );
+        })}
       </>
     );
   };
 
   return (
     <div>
-      <Table>
-        <thead>
-          <TableHead />
-        </thead>
-        <tbody>
-          <TableBody />
-        </tbody>
-      </Table>
+      {dataSource?.length > 0 ? (
+        <Table className="custom-table">
+          <thead>
+            <TableHead />
+          </thead>
+          <tbody>
+            <TableBody />
+          </tbody>
+        </Table>
+      ) : (
+        <>
+          <Table className="custom-table">
+            <thead>
+              <TableHead />
+            </thead>
+          </Table>
+          <Card>
+            <h1
+              className="d-flex justify-content-center"
+              style={{ marginTop: "20px", fontFamily: "Roboto, Arial" }}
+            >
+              No Results Found
+            </h1>
+          </Card>
+        </>
+      )}
     </div>
   );
 };
 
 export default CustomTable;
+
+CustomTable.defaultProps = {
+  tableName: "table",
+  dataSource: [],
+  columns: [],
+  withActions: false,
+  actionList: [],
+  handleActionSelection: () => {},
+};
+
+CustomTable.propTypes = {
+  tableName: PropTypes.string,
+  dataSource: PropTypes.arrayOf(PropTypes.object).isRequired,
+  columns: PropTypes.arrayOf(PropTypes.object).isRequired,
+  withActions: PropTypes.bool,
+  actionList: PropTypes.arrayOf(PropTypes.object),
+  handleActionSelection: PropTypes.func,
+};
--- src/views/products/Products.jsx
+++ src/views/products/Products.jsx
@@ -6,7 +6,54 @@ function Products() {
   return (
     <CommonContainer>
       Products
-      <CustomTable />
+      <CustomTable
+        withActions
+        actionList={[
+          { label: "Edit", icon: "fas fa-pencil-alt" },
+          { label: "Delete", icon: "fas fa-trash" },
+        ]}
+        handleActionSelection={(label) => {
+          console.log("action", label);
+        }}
+        dataSource={[
+          {
+            key: "1",
+            name: "Mike",
+            age: 32,
+            address: "10 Downing Street",
+          },
+          {
+            key: "2",
+            name: "John",
+            age: 42,
+            address: "10 Downing Street",
+          },
+        ]}
+        columns={[
+          {
+            label: "Name",
+            key: "name",
+          },
+          {
+            label: "Age",
+            key: "age",
+          },
+          {
+            label: "Address",
+            key: "address",
+            render: (data) => {
+              return (
+                <div>
+                  {data.name +
+                    ":" +
+                    data.address +
+                    " long string to check overflow with table"}
+                </div>
+              );
+            },
+          },
+        ]}
+      />
     </CommonContainer>
   );
 }
