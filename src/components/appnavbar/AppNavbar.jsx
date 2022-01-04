b7d022b186468dbc42c676f2817d4fd0aab0949e
Shweta <gajakoshshweta30@gmail.com>
2022-01-04 21:49:10 +0530

    fix: dropdown on user icon hover

--- src/components/appnavbar/AppNavbar.jsx
+++ src/components/appnavbar/AppNavbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./appnavbar.scss";
 import cookieService from "../../services/cookies.js";
-import { Layout, Menu, Dropdown, Tooltip } from "antd";
+import { Menu, Dropdown, Tooltip } from "antd";
 
 function AppNavbar() {
   const menu = (
@@ -21,43 +21,39 @@ function AppNavbar() {
   const text = <span>View Tickets</span>;
   return (
     <div className="app-navbar-main">
-      <Layout>
-        <Menu theme="light" mode="horizontal">
-          <Menu.Item>
-            <span
-              style={{
-                color: "#007bfc",
-                fontSize: "12px",
-                //paddingLeft: "10px",
-                fontWeight: "bold",
-              }}
-            >
-              NEWS:{" "}
-            </span>
-          </Menu.Item>
+      <Menu theme="light" mode="horizontal">
+        <Menu.Item>
+          <span
+            style={{
+              color: "#007bfc",
+              fontSize: "12px",
+              //paddingLeft: "10px",
+              fontWeight: "bold",
+            }}
+          >
+            NEWS:{" "}
+          </span>
+        </Menu.Item>
 
-          <Menu.Item style={{ marginLeft: "auto" }}>
-            <Tooltip placement="bottom" title={text}>
-              <i
-                className="fas fa-envelope"
-                style={{ marginRight: "-20px" }}
-              ></i>
-            </Tooltip>
-          </Menu.Item>
-          <Menu.Item>
-            <i className="fas fa-user-circle"></i>
-          </Menu.Item>
+        <Menu.Item style={{ marginLeft: "auto" }}>
+          <Tooltip placement="bottom" title={text}>
+            <i className="fas fa-envelope" style={{ marginRight: "-20px" }}></i>
+          </Tooltip>
+        </Menu.Item>
 
-          <Dropdown overlay={menu} placement="bottomRight" arrow>
-            <Menu.Item
-              style={{ fontSize: "17px", color: "#8692b0", paddingLeft: "0px" }}
-            >
-              <span className="user_Name">Mr.Alex</span>
-              <i className="fas fa-chevron-circle-down"></i>
-            </Menu.Item>
-          </Dropdown>
-        </Menu>
-      </Layout>
+        <Dropdown overlay={menu} placement="bottomRight" arrow>
+          <Menu.Item
+            style={{ fontSize: "17px", color: "#8692b0", paddingLeft: "0px" }}
+          >
+            <i
+              className="fas fa-user-circle"
+              style={{ marginLeft: "20px", marginRight: "15px" }}
+            ></i>
+            <span className="user_Name">Mr.Alex</span>
+            <i className="fas fa-chevron-circle-down"></i>
+          </Menu.Item>
+        </Dropdown>
+      </Menu>
     </div>
   );
 }
