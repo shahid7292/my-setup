6539188e5154f3bf86c272f75a8e4be36d964c92
Shweta Gajakosh <68770910+shweta30g@users.noreply.github.com>
2022-01-05 10:42:48 +0530

    Navbar setup (#2)
    * fix: navbar with news and mail icon and tooltip on hover. wip (account-username-dropdown menu)
    
    * fix: navbar fixed
    
    * feat: routing to support page
    
    * fix: dropdown on user icon hover
    
    * fix: changes in user icon
    
    * refactor: updated appnavbar code
    
    * refactor: updated css
    
    * fix: news margin fixed
    
    * refactor: refactored app-navbar
    
    * feat: added profile page
    
    Co-authored-by: Shweta <gajakoshshweta30@gmail.com>
    Co-authored-by: Shahidraza Moulvi <shahidraza.moulvi@aimedatsolutions.com>

--- src/components/appnavbar/AppNavbar.jsx
+++ src/components/appnavbar/AppNavbar.jsx
@@ -1,19 +1,92 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from "react";
 import "./appnavbar.scss";
 import cookieService from "../../services/cookies.js";
+import { Menu, Dropdown, Tooltip } from "antd";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { Link, useNavigate } from "react-router-dom";
 
 function AppNavbar() {
-  return (
-    <div className="app-navbar-main">
-      App Navbar{" "}
-      <button
+  const navigate = useNavigate();
+  const menu = (
+    <Menu>
+      <Menu.Item>
+        <span
+          onClick={() => {
+            navigate("/app/profile");
+          }}
+        >
+          Profile
+        </span>
+      </Menu.Item>
+      <Menu.Divider />
+      <Menu.Item
         onClick={() => {
           cookieService().clearToken();
           document.location.reload(true);
         }}
       >
         Logout
-      </button>
+      </Menu.Item>
+    </Menu>
+  );
+  const text = <span>View Tickets</span>;
+  return (
+    <div className="app-navbar-main">
+      <div>
+        <span
+          style={{
+            color: "#007bfc",
+            fontSize: "12px",
+            fontWeight: "bold",
+            fontFamily: "Roboto, Arial",
+          }}
+        >
+          NEWS:{" "}
+        </span>
+      </div>
+
+      <div style={{ marginLeft: "auto" }}>
+        <div style={{ padding: "3px 20px 0px 0px" }}>
+          <Tooltip placement="bottom" title={text}>
+            <Link to="support">
+              <i
+                className="fas fa-envelope"
+                style={{
+                  marginRight: "-20px",
+                  color: "#8692b0",
+                  fontSize: "20px",
+                }}
+              ></i>
+            </Link>
+          </Tooltip>
+        </div>
+      </div>
+      <Dropdown
+        overlay={menu}
+        placement="bottomRight"
+        trigger={["hover", "click"]}
+        arrow
+        overlayClassName={"navbar-dropdown"}
+      >
+        <div
+          style={{
+            fontSize: "17px",
+            color: "#8692b0",
+            paddingRight: "30px",
+          }}
+        >
+          <i
+            className="fas fa-user-circle"
+            style={{ marginLeft: "20px", marginRight: "15px" }}
+          ></i>
+          <span className="user_Name">Mr.Alex</span>
+          <span style={{ position: "absolute", paddingTop: "3px" }}>
+            <MdKeyboardArrowDown />
+          </span>
+        </div>
+      </Dropdown>
     </div>
   );
 }
---
+++ src/views/profile/Profile.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import CommonContainer from "../../helpers/CommonContainer.jsx";
+
+function Profile() {
+  return <CommonContainer>Profile</CommonContainer>;
+}
+
+export default Profile;
